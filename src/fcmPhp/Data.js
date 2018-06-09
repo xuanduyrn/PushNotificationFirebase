eb9c521b5bc70497f3c47b085ebd252fe474711a
duydevit <xuanduyrn@gmail.com>
2018-06-09 12:35:48 +0700

    s

--- src/fcmPhp/Data.js
+++ src/fcmPhp/Data.js
@@ -12,9 +12,9 @@ export default class MyComponent extends Component {
   render() {
     const { item } = this.props;
     return (
-      <TouchableOpacity style={{width: 200, height: 40, flexDirection: 'column', backgroundColor: 'green', justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity style={{width: 200, height: 40, flexDirection: 'column', backgroundColor: 'green', justifyContent: 'center', alignItems: 'center', marginTop: 10}}>
         <Text style={{}}>{item.title}</Text>
-        <Text style={{}}>{item.message}</Text>
+        <Text style={{}}>{item.body}</Text>
       </TouchableOpacity>
     );
   }
